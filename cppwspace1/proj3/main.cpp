944636fbbd8c57582f714975224d558c52e31484
Abhishek Kumar Singh <abhishek.singh@anu.edu.au>
2019-11-07 22:59:42 -0500

    First commit

---
+++ cppwspace1/proj3/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    int fav_num;
+    std:: cout <<"Hello world , enter any number between 1 and 100 \n";
+    std:: cin >>  fav_num;
+    std::  cout <<"The number you entered is  :" <<fav_num;
+    
+    return 0;
+}
